66bc87d63c7edc6de6f9baebc4ed83965dca63c7
Ana A. Ribeiro <190616861+AlicyRibeiro@users.noreply.github.com>
2026-02-25 09:06:15 -0300

    Add header comment to print_Hello.c
    Added a detailed header comment to print_Hello.c.

--- lab01-makefile/src/print_Hello.c
+++ lab01-makefile/src/print_Hello.c
@@ -1,3 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  <print_Hello.c>
+ *
+ *    Description:  Código desenvolvido para a prática da disciplina
+ *                  Organização de Computadores e Linguagens de Montagem II.
+ *
+ *        Version:  1.0
+ *        Created:  2025
+ *       Revision:  none
+ *
+ *         Author:  Ana Alicy Ribeiro
+ *        Contact:  github.com/<AlicyRibeiro>
+ *   Organization:  Universidade Federal do Ceará - Campus Quixadá
+ *
+ *   GitHub Repo:   organizacao-computadores-ii-labs
+ *
+ *
+ *   Notes:
+ *   Este código foi desenvolvido exclusivamente para fins educacionais.
+ *
+ * =====================================================================================
+ */
+
 #include "print_Hello.h"
 
 void print_Hello(void){
